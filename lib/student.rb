3e38c51bf3c22ac89a1505338df96fbb4b7fb196
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-03-12 19:35:15 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -70,7 +70,6 @@ class Student
 
     tenth_grade_array = DB[:conn].execute(sql).map do |row|
         self.new_from_db(row)
-        #tenth_grade_array << student
       end
       tenth_grade_array[0..num-1]
   end
